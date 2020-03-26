3f06a789ca631498ae9b4d876354bce9dcb56e62
migue1994 <migue24co@gmail.com>
2020-03-26 17:59:58 -0500

    Front parcialmente terminado

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,40 @@ var mock = apiclient;
 
 var app = (function () {
 
-    var getCountriesTable = function (data) {
+    var getProvincesTable = function (data) {
         console.info(data);
+        var covidData = data;
+        $("#provinces").empty();
+        covidData.map(function (c) {
+            $("#provinces").append(
+                "<tr> " +
+                "<td>" + c.province + "</td> " +
+                "<td>" + c.deaths+"</td>" +
+                "<td>"+c.confirmed+"</td>" +
+                "<td>"+c.recovered+"</td>" +
+                "</tr>"
+            );
+        });
+    };
+
+    var getProvidences = function (name) {
+        $("#pais").text(name);
+        mock.getCountries("api/allCountries/"+name, getProvincesTable);
+    };
+
+    var getCountriesTable = function (data) {
+        var covidData = data;
+        $("#status").empty();
+        covidData.map(function (c) {
+            $("#status").append(
+                "<tr> " +
+                "<td><a onclick='app.getProvidences(\""+c.name+"\")'>" + c.name + "</a></td> " +
+                "<td>" + c.numDeaths + "</td> " +
+                "<td>" + c.numInfected+"</td>" +
+                "<td>"+c.numCured+"</td>" +
+                "</tr>"
+            );
+        });
     };
 
     var getCountries = function () {
@@ -11,7 +43,8 @@ var app = (function () {
     };
 
     return {
-        getCountries : getCountries
+        getCountries : getCountries,
+        getProvidences : getProvidences
     };
 
 })();
