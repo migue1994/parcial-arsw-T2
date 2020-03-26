193888a770ba7eadac1fc57d0e2022d85c23f4ae
migue1994 <migue24co@gmail.com>
2020-03-26 07:27:03 -0500

    Avance

--- src/main/java/edu/eci/arsw/covid19/parcial/controller/RestController.java
+++ src/main/java/edu/eci/arsw/covid19/parcial/controller/RestController.java
@@ -1,4 +1,12 @@
 package edu.eci.arsw.covid19.parcial.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api")
 public class RestController {
+
+
+
 }
